a43b8f58eee48b5f742b312d091f13e16c78d143
hasindi <hmudithya@gmail.com>
2022-11-13 20:25:28 +0530

    Added other methods

--- assignments/JS/Que/assets/js/script.js
+++ assignments/JS/Que/assets/js/script.js
@@ -1,12 +1,36 @@
 var input = {
-    persons:[
-        {name: "Pawani", color:"red"},
-        {name: "Dilki", color:"green"},
-        {name: "Nuwan", color:"orange"}
+    persons: [
+        {name: "Pawani", color: "red"},
+        {name: "Dilki", color: "green"},
+        {name: "Nuwan", color: "orange"}
     ],
 
-    nextPerson:function (){
+    nextPerson: function () {
         var lastPerson = this.persons.pop();
         this.persons.unshift(lastPerson);
     }
 }
+
+renderDivs();
+
+function renderDivs() {
+    $(".sec2").empty();
+    for (let i = 0; i < input.persons.length; i++) {
+        $(".sec2").append(`<div style="background-color: ${input.persons[i].color}"><h5>${input.persons[i].name}</h5></div>`);
+    }
+    input.nextPerson();
+}
+
+setInterval(renderDivs, 400);
+
+$("#btn").click(function () {
+    let name = $("#name").val();
+    let color = $("#color").val();
+
+    var Object = {
+        name: name,
+        color: color
+    }
+
+    input.persons.unshift(Object);
+});
