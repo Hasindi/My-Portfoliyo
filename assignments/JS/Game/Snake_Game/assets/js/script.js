b7e2fabe56792fd78ff3cef76012207c7a9a348c
hasindi <hmudithya@gmail.com>
2022-11-24 21:18:23 +0530

    Make change to food place.

--- assignments/JS/Game/Snake_Game/assets/js/script.js
+++ assignments/JS/Game/Snake_Game/assets/js/script.js
@@ -1,12 +1,20 @@
-var blockSize = 8;
+var blockSize = 27;
+var column = 30;
+var row = 20;
 var board;
 var grid;
 
 
 window.onload = function () {
     board = document.getElementById("board");
+
+    /*game boad width height*/
+    board.height = row * blockSize;
+    board.width = column * blockSize;
+
     grid = board.getContext("2d");
 
+    setFoodPlace();
     update();
 }
 
@@ -17,15 +25,28 @@ var snakeY = blockSize * 7;
 
 /*snake food*/
 /*10=place*/
-var foodX = blockSize * 20;
-var foodY = blockSize * 10;
+var foodX;
+var foodY;
 
+
+/*snake and food*/
 function update() {
+    /*game board style*/
+    grid.fillStyle = "black";
+    grid.fillRect(0, 0, board.width, board.height);
+
     /*snake head style*/
     grid.fillStyle = "green";
-    grid.fillRect(snakeX, snakeY, blockSize, blockSize);
+    grid.fillRect(snakeX, snakeY, blockSize, blockSize);/*snake head style*/
 
     /*food style*/
     grid.fillStyle = "crimson";
     grid.fillRect(foodX, foodY, blockSize, blockSize);
+}
+
+
+/*change food place*/
+function setFoodPlace() {
+    foodX = Math.floor(Math.random() * column) * blockSize;
+    foodY = Math.floor(Math.random() * row) * blockSize;
 }
