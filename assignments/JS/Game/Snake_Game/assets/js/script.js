17c0bc52239cd9f8c5872c4e89ff0f85ef63a98a
hasindi <hmudithya@gmail.com>
2022-11-24 22:19:17 +0530

    Make snake touch food.

--- assignments/JS/Game/Snake_Game/assets/js/script.js
+++ assignments/JS/Game/Snake_Game/assets/js/script.js
@@ -4,6 +4,19 @@ var row = 20;
 var board;
 var grid;
 
+/*snake head*/
+/*7=place*/
+var snakeX = blockSize * 7;
+var snakeY = blockSize * 7;
+
+/*snake food*/
+/*10=place*/
+var foodX;
+var foodY;
+
+/*snake speed*/
+var timerX=0;
+var timerY=0;
 
 window.onload = function () {
     board = document.getElementById("board");
@@ -15,19 +28,10 @@ window.onload = function () {
     grid = board.getContext("2d");
 
     setFoodPlace();
-    update();
+    document.addEventListener("keyup", setSnakeMove);
+    /*update();*/
+    setInterval(update, 1000/10);
 }
-
-/*snake head*/
-/*7=place*/
-var snakeX = blockSize * 7;
-var snakeY = blockSize * 7;
-
-/*snake food*/
-/*10=place*/
-var foodX;
-var foodY;
-
 
 /*snake and food*/
 function update() {
@@ -35,13 +39,41 @@ function update() {
     grid.fillStyle = "black";
     grid.fillRect(0, 0, board.width, board.height);
 
-    /*snake head style*/
-    grid.fillStyle = "green";
-    grid.fillRect(snakeX, snakeY, blockSize, blockSize);/*snake head style*/
-
     /*food style*/
     grid.fillStyle = "crimson";
     grid.fillRect(foodX, foodY, blockSize, blockSize);
+
+    /*snake  touch foods*/
+    if (snakeX==foodX&&snakeY==foodY){
+        setFoodPlace();
+    }
+
+    /*snake head style*/
+    grid.fillStyle = "green";
+    snakeX+=timerX* blockSize;
+    snakeY+=timerY* blockSize;
+    grid.fillRect(snakeX, snakeY, blockSize, blockSize);
+}
+
+/*snake move*/
+function setSnakeMove(s) {
+    if (s.code == "ArrowUp"){
+        timerX=0;
+        timerY=-1;
+    }
+    else if (s.code == "ArrowDown"){
+        timerX=0;
+        timerY=1;
+    }
+    else if (s.code == "ArrowLeft"){
+        timerX=-1;
+        timerY=0;
+    }
+    else if (s.code == "ArrowRight"){
+        timerX=1;
+        timerY=0;
+    }
+
 }
 
 
