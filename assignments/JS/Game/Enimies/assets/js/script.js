9563be65dda4f470c59e2399bd46dc442955daf7
hasindi <hmudithya@gmail.com>
2022-11-27 18:22:20 +0530

    set Run Animation for girl

--- assignments/JS/Game/Enimies/assets/js/script.js
+++ assignments/JS/Game/Enimies/assets/js/script.js
@@ -1,18 +1,53 @@
 var girl = document.getElementById("girl");
 
-var idleImageNumber=0;
-var idleAnimationNumber=0;
+var idleImageNumber = 0;
+var idleAnimationNumber = 0;
+var runImageNumber = 0;
+var runAnimationNumber = 0;
 
-function idleAnimation(){
+
+/*girl Animation*/
+function idleAnimation() {
     idleImageNumber = idleImageNumber + 1;
 
-    if (idleImageNumber===11){
-        idleImageNumber=1;
+    if (idleImageNumber === 11) {
+        idleImageNumber = 1;
     }
 
-    girl.src="assets/Idle ("+idleImageNumber+").png";
+    girl.src = "assets/Idle (" + idleImageNumber + ").png";
 }
 
-function idleStartAnimation(){
+function idleStartAnimation() {
     idleAnimationNumber = setInterval(idleAnimation, 200);
+}
+
+
+/*run for girl*/
+function runAnimation() {
+    runImageNumber = runImageNumber + 1;
+
+    if (runImageNumber === 11) {
+        runImageNumber = 1;
+    }
+
+
+    girl.src = "assets/Run (" + runImageNumber + ").png";
+
+}
+
+function runAnimationStart() {
+    runAnimationNumber = setInterval(runAnimation, 100);
+    clearInterval(idleAnimationNumber);
+
+}
+
+/*Enter key Press*/
+function keyCheck(event) {
+    var keyCode = event.which;
+
+    if (keyCode === 13) {
+        if (runAnimationNumber === 0) {
+            runAnimationStart();
+        }
+    }
 }
